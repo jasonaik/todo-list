aa892b82311e29f2e2e71247ed99f21bfa878773
Jason Aik <jasonaik39@gmail.com>
2021-05-21 15:57:40 +0800

    Updated Database to DATABASE_URL1

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///lists.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1",  "sqlite:///lists.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
